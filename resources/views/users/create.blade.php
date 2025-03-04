2f4dc1c4c5d43d7b943c616b5319444a81acf806
cleberjesus2 <cleberjesus493@gmail.com>
2025-03-04 16:17:57 -0300

    View create para  criação de Usuarios

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -5,7 +5,7 @@
     <title>Novo Usuário</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
-<body class="bg-light">
+<body class="bg-light"> 
     <div class="container my-5"> 
         <h2 class="text-center mb-4">Criar Usuário</h2>
         <form action="{{ route('usuarios.store') }}" method="post">
