c16be6f4ec70046ac2fef9a05561d4a5fb183d55
cleberjesus2 <cleberjesus493@gmail.com>
2025-03-04 14:15:24 -0300

    Controller de Produtos

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -19,7 +19,7 @@ class ProductController extends Controller
      * Show the form for creating a new resource.
      */
     public function create()
-    {
+    { 
         return view('products.create');
     }
 
